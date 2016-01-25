65b0c68715e6679f8427bcdd7c7ee57c4d7d25b3
Vova <acpfog@gmail.com>
2016-01-25 21:27:16 +0200

    Changed paying_debt_off_in_a_year_bisection.py ( usinge a procedure )

--- 6.00.1x Files/Week 2/Problem Set 2/paying_debt_off_in_a_year_bisection.py
+++ 6.00.1x Files/Week 2/Problem Set 2/paying_debt_off_in_a_year_bisection.py
@@ -13,28 +13,31 @@
 #
 #Write a program that uses these bounds and bisection search (for more info check out the Wikipedia page on bisection search) to find the smallest monthly payment to the cent (no more multiples of $10) such that we can pay off the debt within a year. Try it out with large inputs, and notice how fast it is (try the same large inputs in your solution to Problem 2 to compare!). Produce the same return value as you did in Problem 2.
 
-balance = 320000
-annualInterestRate = 0.2
+balance = 999999
+annualInterestRate = 0.18
+
+def doAnnualPayments ( balance , fixedPayment, monthlyInterestRate ):
+    for month in range ( 12 ):
+        monthlyUnpaidBalance = balance - fixedPayment
+        balance = monthlyUnpaidBalance + ( monthlyUnpaidBalance * monthlyInterestRate )
+    return balance
 
 monthlyInterestRate = annualInterestRate / 12.0
-lowBond = balance / 12.0
-upperBond = ( balance * ( 1 + monthlyInterestRate ) ** 12 ) / 12.0
+minPayment = balance / 12.0
+maxPayment = ( balance * ( 1 + monthlyInterestRate ) ** 12 ) / 12.0
 
 while True:
 
-    tmpBalance = balance
-    monthlyPayment = ( lowBond + upperBond ) / 2
+    approxPayment = ( minPayment + maxPayment ) / 2
+    net = doAnnualPayments ( balance , approxPayment , monthlyInterestRate )
 
-    for month in range ( 1 , 13 ):
-        monthlyUnpaidBalance = tmpBalance - monthlyPayment
-        tmpBalance = monthlyUnpaidBalance + ( monthlyUnpaidBalance * monthlyInterestRate )
-
-    if tmpBalance > 0:
-        lowBond = monthlyPayment
+    if net > 0:
+        minPayment = approxPayment
     else:
-        upperBond = monthlyPayment
+        maxPayment = approxPayment
 
-    if tmpBalance > 0 and tmpBalance < 0.001:
+    if net < 0 and net > -0.001:
         break
 
-print "Lowest Payment: %s" % round ( monthlyPayment , 2 )
+print "Lowest Payment: %s" % round ( approxPayment , 2 )
+
