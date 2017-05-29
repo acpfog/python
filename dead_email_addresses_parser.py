bb3790a8cffe69f0955032633becbcfa3b0222f0
Volodymyr Larkin <acpfog@gmail.com>
2017-05-29 07:54:31 +0200

    Comments added

--- dead_email_addresses_parser.py
+++ dead_email_addresses_parser.py
@@ -6,16 +6,20 @@ import datetime
 from sys import stdin
 from email.Parser import Parser
 
+# load a message data from stdin and extract a subject of the message 
 message = Parser().parse(stdin)
 subject = message.get('Subject')
 
+# start processing if the subject about a delivery failure
 if subject == 'Delivery Status Notification (Failure)':
 
+# open the log file, then write a starting record
     f = open('/storage/project/logs/dead_email_addresses_parser.log', 'a')
 
     now = datetime.datetime.now()
     print >>f, "%s #1 A message with Failure Delivery Notification was detected" % (str(now))
 
+# extract a body of the message
     if not message.is_multipart():
         body = message.get_payload(decode=True)
     else:
@@ -23,12 +27,15 @@ if subject == 'Delivery Status Notification (Failure)':
             if part.get_content_type() == 'text/plain':
                 body = part.get_payload(decode=True)
 
+# generate an expression and look for e-mail addresses in the body
     r = re.compile(r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)')
     results = r.findall(body)    
 
+# connect to a database
     db = MySQLdb.connect("127.0.0.1","db_user","db_pass","db_name" )
     cursor = db.cursor()
-    
+
+# check and store each new e-mail address in the dead_email_addresses table and write to the log
     for x in results:
 
         now = datetime.datetime.now()
